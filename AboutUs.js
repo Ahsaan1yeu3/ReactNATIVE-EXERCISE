9155c961acbb07e4104588850cc0a5d3d567d07d
Ahsaan hassan <87330404+Ahsaan1yeu3@users.noreply.github.com>
2023-04-03 23:55:56 +0500

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,11 @@
+export default function AboutUs() {
+    return (
+        <div>
+            <h2>About Baham</h2>
+            <p>A great description can make your app stand out in the Microsoft Store and help encourage customers
+                 to download it. The description you enter when submitting your app is displayed in your
+                  app's Store listing. The first few lines may also be displayed in search results and 
+                  algorithm lists in the Store.</p>
+        </div>
+    );
+}
